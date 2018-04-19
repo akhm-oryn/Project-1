9c6764650e4ff54c6e36d3f7d22c7fdfddd59fd1
Akhmet <ahmetoryn@gmail.com>
2018-04-19 15:59:18 +0600

    listing images with file manager

--- Project-1/Project-1/ViewController.swift
+++ Project-1/Project-1/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let fm = FileManager.default
+        //объявление константы фм, с помощью которой в будушем можно будет работать с файловой системой
+        let path = Bundle.main.resourcePath!
+        // объявление константы, с помощью которой впредь можно обращаться к файлам из бандла (бандл - директория, содержащяя в себе все assests (прилагающие файлы) и скомпелированную программу. Простыми словами, находим путь к файлам
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        //восклицательный знак (!) является принудительным извлечением значения из опционала
+        //извлекаем значение из предыдущей константы path
+        //при неудаче случится краш из-за "!"
+        
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+                //здесь используется цикл
+            }
+        }
+    print(pictures)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
